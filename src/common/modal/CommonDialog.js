09384a95684748a3dd68b4900ae878ec1f21408e
Anuj Gupta <vedanuj@yahoo.com>
2021-07-11 18:57:55 +0530

    add dialog form

--- src/common/modal/CommonDialog.js
+++ src/common/modal/CommonDialog.js
@@ -5,6 +5,8 @@ import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
 import PropTypes from "prop-types";
 import CancelRoundedIcon from "@material-ui/icons/CancelRounded";
+import { Grid, Typography, IconButton } from "@material-ui/core";
+
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -34,7 +36,23 @@ const CommonDialog = (props) => {
       }}
     >
       <Fade in={props.open} style={{ width: "50vw" }}>
-        <div className={classes.paper}>{props.chilren}</div>
+        <div className={classes.paper}>
+          <Grid container style={{ alignItems: "center" }}>
+            <Grid item xs={12} md={6}>
+              <Typography>{props.title}</Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <IconButton
+                style={{ float: "right" }}
+                onClick={props.onCloseClickListener}
+                color="primary"
+              >
+                <CancelRoundedIcon />
+              </IconButton>
+            </Grid>
+            <Grid>{props.children}</Grid>
+          </Grid>
+        </div>
       </Fade>
     </Modal>
   );
