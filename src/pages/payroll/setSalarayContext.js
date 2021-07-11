7fe8a8ffe7ecd284078ccae4b7d5c29a6ac79a42
Anuj Gupta <vedanuj@yahoo.com>
2021-07-11 18:57:55 +0530

    add dialog form

--- src/pages/payroll/setSalarayContext.js
+++ src/pages/payroll/setSalarayContext.js
@@ -1,28 +1,39 @@
 import React, { createContext, useEffect, useState } from "react";
 import CommonDialog from "../../common/modal/CommonDialog";
 
+
 const SetSallaryContext = createContext();
 
 const SetSallaryProvider = ({ children }) => {
   const [isOpen, setIsopen] = useState(false);
-  const [renderComponent, setEnderComponent] = useState(null);
+  const [RenderComponent, setEnderComponent] = useState(null);
   const [dialogTitle, setDialogTitle] = useState("");
-  const OpenDialogWithForm = () => {
-    setIsopen(true);
-
-    setDialogTitle("working")
+ 
+  const OpenDialogWithForm = (component) => {
+      setEnderComponent(component[0]);
+      setDialogTitle(component[1]);
+      setIsopen(true);
   };
+  
   const CloseDialogWithForm = () => {
     setIsopen(false);
     setEnderComponent(null);
   };
+  const AllowanceSubmitHandler = (e) =>{
+    console.log(e)
+  }
+
 
   return (
-    <SetSallaryContext.Provider value={{ OpenDialogWithForm }}>
+    <SetSallaryContext.Provider value={{ OpenDialogWithForm, AllowanceSubmitHandler }}>
       <>
         {children}
-        <CommonDialog open={isOpen} onCloseClickListener={CloseDialogWithForm} title={dialogTitle}>
-          {renderComponent}
+        <CommonDialog
+          open={isOpen}
+          onCloseClickListener={CloseDialogWithForm}
+          title={dialogTitle}
+        >
+         {RenderComponent}
         </CommonDialog>
       </>
     </SetSallaryContext.Provider>
