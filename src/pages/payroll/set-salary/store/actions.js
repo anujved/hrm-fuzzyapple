845bec47da1571da4bb961494a2c17fc82cbc104
Anuj Gupta <vedanuj@yahoo.com>
2021-07-11 19:09:19 +0530

    dialog form is ready

--- src/pages/payroll/set-salary/store/actions.js
+++ src/pages/payroll/set-salary/store/actions.js
@@ -1,3 +1,6 @@
 import { FETCH_EMPLOYEE_REQUEST } from "./constants";
 
 export const fetchEmployee = () => ({ type: FETCH_EMPLOYEE_REQUEST });
+
+export const editEmployeeSalary = (formData) => ({ type: FETCH_EMPLOYEE_REQUEST,payload:formData });
+
